96c14157f83267b0ebd0a16d565a2054ce803a27
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-04-24 20:18:27 +0200

    CS

--- src/main/kotlin/com/kbalazsworks/ssp_ai_backend/domain/entities/JiraBoard.kt
+++ src/main/kotlin/com/kbalazsworks/ssp_ai_backend/domain/entities/JiraBoard.kt
@@ -3,12 +3,7 @@ package com.kbalazsworks.ssp_ai_backend.domain.entities
 import com.kbalazsworks.ssp_ai_backend.db.tables.JiraBoards
 import java.time.LocalDateTime
 
-data class JiraBoard(
-    val id: Long?,
-    val companyId: Long,
-    val name: String,
-    val createdAt: LocalDateTime,
-) {
+data class JiraBoard(val id: Long?, val companyId: Long, val name: String, val createdAt: LocalDateTime) {
     companion object {
         val TABLE_NAME = JiraBoards.JIRA_BOARDS
     }
