f4f83b6c642d93e353ff6907a0db08c36709acc1
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-04-26 19:08:56 +0200

    AbstractRepository.toPgVectorField added

--- src/main/kotlin/com/kbalazsworks/ssp_ai_backend/domain/repositories/AbstractRepository.kt
+++ src/main/kotlin/com/kbalazsworks/ssp_ai_backend/domain/repositories/AbstractRepository.kt
@@ -2,9 +2,13 @@ package com.kbalazsworks.ssp_ai_backend.domain.repositories
 
 import com.kbalazsworks.ssp_ai_backend.common.services.JooqService
 import com.kbalazsworks.ssp_ai_backend.db.tables.JiraTicketEmbeddings
+import com.pgvector.PGvector
+import org.jooq.impl.DSL
 import org.springframework.stereotype.Repository
 
 @Repository
 abstract class AbstractRepository(private val jooqService: JooqService) {
     val jiraTicketEmbeddingTable = JiraTicketEmbeddings.JIRA_TICKET_EMBEDDINGS
+
+    fun toPgVectorField(embedding: PGvector?) = DSL.field("?::vector", PGvector::class.java, embedding?.toString())
 }
--- src/main/kotlin/com/kbalazsworks/ssp_ai_backend/domain/repositories/JiraIssueEmbeddingRepository.kt
+++ src/main/kotlin/com/kbalazsworks/ssp_ai_backend/domain/repositories/JiraIssueEmbeddingRepository.kt
@@ -4,31 +4,18 @@ import com.kbalazsworks.ssp_ai_backend.common.services.JooqService
 import com.kbalazsworks.ssp_ai_backend.db.tables.references.JIRA_TICKET_EMBEDDINGS
 import com.kbalazsworks.ssp_ai_backend.domain.entities.JiraIssueEmbedding
 import com.kbalazsworks.ssp_ai_backend.domain.exceptions.JiraTicketEmbeddingException
-import com.pgvector.PGvector
-import org.jooq.impl.DSL
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 
 @Repository
 class JiraIssueEmbeddingRepository(private val jooqService: JooqService) : AbstractRepository(jooqService) {
-    companion object {
-        private val logger = LoggerFactory.getLogger(JiraIssueEmbeddingRepository::class.java)
-    }
-
     fun save(jiraIssueEmbedding: JiraIssueEmbedding): JiraIssueEmbedding {
         val entity: JiraIssueEmbedding? = jooqService.getDslContext()
             .insertInto(JIRA_TICKET_EMBEDDINGS)
             .set(JIRA_TICKET_EMBEDDINGS.JIRA_SPRINT_ID, jiraIssueEmbedding.jiraSprintId)
             .set(JIRA_TICKET_EMBEDDINGS.RAW_JSON, jiraIssueEmbedding.rawJson)
             .set(JIRA_TICKET_EMBEDDINGS.OPENAI_COMPATIBLE_TEXT, jiraIssueEmbedding.openaiCompatibleText)
-            .set(
-                JIRA_TICKET_EMBEDDINGS.EMBEDDING1536,
-                DSL.field("?::vector", PGvector::class.java, jiraIssueEmbedding.embedding1536?.toString())
-            )
-            .set(
-                JIRA_TICKET_EMBEDDINGS.EMBEDDING3072,
-                DSL.field("?::vector", PGvector::class.java, jiraIssueEmbedding.embedding3072?.toString())
-            )
+            .set(JIRA_TICKET_EMBEDDINGS.EMBEDDING1536, toPgVectorField(jiraIssueEmbedding.embedding1536))
+            .set(JIRA_TICKET_EMBEDDINGS.EMBEDDING3072, toPgVectorField(jiraIssueEmbedding.embedding3072))
             .set(JIRA_TICKET_EMBEDDINGS.CREATED_AT, jiraIssueEmbedding.createdAt)
             .returning()
             .fetchOneInto(JiraIssueEmbedding::class.java)
