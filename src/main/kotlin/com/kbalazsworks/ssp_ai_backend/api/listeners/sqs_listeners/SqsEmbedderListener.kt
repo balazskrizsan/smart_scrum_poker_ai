544c4d80e79563b51663ea7ac696cb5333537fc9
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-05-19 20:16:49 +0200

    SqsEmbedderListener @ConditionalOnProperty added

--- src/main/kotlin/com/kbalazsworks/ssp_ai_backend/api/listeners/sqs_listeners/SqsEmbedderListener.kt
+++ src/main/kotlin/com/kbalazsworks/ssp_ai_backend/api/listeners/sqs_listeners/SqsEmbedderListener.kt
@@ -1,12 +1,15 @@
 package com.kbalazsworks.ssp_ai_backend.api.listeners.sqs_listeners
 
 import com.kbalazsworks.ssp_ai_backend.common.services.ApplicationPropertiesService
+import com.kbalazsworks.ssp_ai_backend.common.services.ApplicationPropertiesService.Companion.AWS_SQS_LISTENER_ENABLED
 import com.kbalazsworks.ssp_ai_backend.domain.sqs_module.services.EmbedderQueueHandlerService
 import io.awspring.cloud.sqs.annotation.SqsListener
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Component
 
-@Service
+@Component
+@ConditionalOnProperty(name = [AWS_SQS_LISTENER_ENABLED])
 class SqsEmbedderListener(private val embedderQueueHandlerService: EmbedderQueueHandlerService) {
     companion object {
         private val logger = LoggerFactory.getLogger(this::class.java)
--- src/main/kotlin/com/kbalazsworks/ssp_ai_backend/common/services/ApplicationPropertiesService.kt
+++ src/main/kotlin/com/kbalazsworks/ssp_ai_backend/common/services/ApplicationPropertiesService.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service
 class ApplicationPropertiesService {
     companion object{
         const val SCHEDULERS_FAKE_TICKET_SCHEDULER_ENABLED = "schedulers.fake-ticket-scheduler.enabled"
+        const val AWS_SQS_LISTENER_ENABLED = "aws.sqs.listener.enabled"
         const val AWS_SQS_QUEUE_NAME = "aws.sqs.queue-name"
         const val AWS_SQS_LISTENER_MAX_MESSAGES_PER_POLL = "aws.sqs.listener.max-messages-per-poll"
     }
@@ -24,6 +25,7 @@ class ApplicationPropertiesService {
 
     @Value("\${$SCHEDULERS_FAKE_TICKET_SCHEDULER_ENABLED}") lateinit var schedulersFakeTicketSchedulerEnabled: String
 
+    @Value("\${$AWS_SQS_LISTENER_ENABLED}")  lateinit var awsSqsListenerEnabled: String
     @Value("\${aws.sqs.queue-url}") lateinit var awsSqsQueueUrl: String
     @Value("\${aws.sqs.queue-name}") lateinit var awsSqsQueueName: String
     @Value("\${aws.sqs.acces-key-id}") lateinit var awsSqsAccessKeyId: String
