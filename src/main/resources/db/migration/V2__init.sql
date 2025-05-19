35fb63a4005036d159fed87d7955dc1587e33533
Balazs Krizsan <krizsan.balazs@gmail.com>
2025-05-19 18:10:03 +0200

    V2__init.sql vector store unique filed fix

--- src/main/resources/db/migration/V2__init.sql
+++ src/main/resources/db/migration/V2__init.sql
@@ -49,12 +49,19 @@ CREATE TABLE vector_store_1536
     jira_issue_id   BIGINT NULL REFERENCES jira_issues (id) ON DELETE CASCADE,
     vector_model_id BIGINT NOT NULL REFERENCES vector_models (id) ON DELETE CASCADE,
     embedding       VECTOR(1536) NOT NULL,
-    UNIQUE (question_id, jira_issue_id, vector_model_id),
     CHECK (
         (question_id IS NOT NULL AND jira_issue_id IS NULL) OR
         (question_id IS NULL AND jira_issue_id IS NOT NULL)
         )
 );
+
+CREATE UNIQUE INDEX unique__vector_store_1536__question_id__vector_model_id
+    ON vector_store_1536 (question_id, vector_model_id)
+    WHERE jira_issue_id IS NULL;
+
+CREATE UNIQUE INDEX unique__vector_store_1536__jira_issue_id__vector_model_id
+    ON vector_store_1536 (jira_issue_id, vector_model_id)
+    WHERE question_id IS NULL;
 
 CREATE TABLE vector_store_3072
 (
@@ -62,9 +69,16 @@ CREATE TABLE vector_store_3072
     jira_issue_id   BIGINT NULL REFERENCES jira_issues (id) ON DELETE CASCADE,
     vector_model_id BIGINT NOT NULL REFERENCES vector_models (id) ON DELETE CASCADE,
     embedding       VECTOR(3072) NOT NULL,
-    UNIQUE (question_id, jira_issue_id, vector_model_id),
     CHECK (
         (question_id IS NOT NULL AND jira_issue_id IS NULL) OR
         (question_id IS NULL AND jira_issue_id IS NOT NULL)
         )
 );
+
+CREATE UNIQUE INDEX unique__vector_store_3072__question_id__vector_model_id
+    ON vector_store_3072 (question_id, vector_model_id)
+    WHERE jira_issue_id IS NULL;
+
+CREATE UNIQUE INDEX unique__vector_store_3072__jira_issue_id__vector_model_id
+    ON vector_store_3072 (jira_issue_id, vector_model_id)
+    WHERE question_id IS NULL;
